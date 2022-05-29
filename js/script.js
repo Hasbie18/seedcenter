67c6d5df22162cf3d168ccc8a83e88c21a7ce802
Hasbie18 <m.sultanha@gmail.com>
2022-05-29 19:19:56 +0700

    adding carousel, link, and modals

--- js/script.js
+++ js/script.js
@@ -17,3 +17,22 @@ window.onscroll = function(){
     nav.classList.remove('navbar-fixed');
   }
 }
+
+
+// modal
+window.addEventListener('DOMContentLoaded', () =>{
+  const overlay = document.querySelector('#overlay')
+  const delBtn = document.querySelector('#show-modal')
+  const closeBtn = document.querySelector('#close-modal')
+  const closeBtn2 = document.querySelector('#close-modal2')
+
+  const toggleModal = () => {
+      overlay.classList.toggle('hidden')
+      overlay.classList.toggle('flex')
+  }
+
+  delBtn.addEventListener('click', toggleModal)
+
+  closeBtn.addEventListener('click', toggleModal)
+  closeBtn2.addEventListener('click', toggleModal)
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  content: ['index.html'],
+  content: [
+    'index.html',
+  ],
   theme: {
     screens:{
       'sm' : '640px',
